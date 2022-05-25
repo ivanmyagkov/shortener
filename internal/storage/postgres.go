97ab0f1a96aeb3b98f22cf2eb633351cae7e0aa0
ivan_miagkov_1 <myagkovivan1@gmail.com>
2022-05-25 22:39:38 +0300

    increment 10

---
+++ internal/storage/postgres.go
@@ -0,0 +1,50 @@
+package storage
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+
+	"github.com/ivanmyagkov/shortener.git/internal/interfaces"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func NewDb(psqlConn string) *Storage {
+	db, err := sql.Open("postgres", psqlConn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to DB!")
+	return &Storage{
+		db: db,
+	}
+}
+
+func (D *Storage) GetURL(shortURL string) (string, error) {
+	panic("implement me")
+}
+
+func (D *Storage) GetAllURLsByUserID(userID string) ([]interfaces.ModelURL, error) {
+	panic("implement me")
+}
+
+func (D *Storage) SetShortURL(userID, shortURL, baseURL string) error {
+	panic("implement me")
+}
+
+func (D *Storage) Ping() error {
+	return D.db.Ping()
+}
+
+func (D *Storage) Close() {
+	D.Close()
+}
