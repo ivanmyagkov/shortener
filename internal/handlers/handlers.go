be05e4f15812dfd9e7bbb73ce73f1615e4d2cdd1
ivan_miagkov_1 <myagkovivan1@gmail.com>
2022-05-28 13:30:57 +0300

    increment 13 fix

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -42,11 +42,15 @@ func (s Server) PostURL(c echo.Context) error {
 	}
 
 	ShortURL, err := s.shortenURL(userID, string(body))
-	if errors.Is(err, interfaces.ErrAlreadyExists) {
-		return c.String(http.StatusConflict, ShortURL)
-	} else {
-		return c.NoContent(http.StatusBadRequest)
+
+	if err != nil {
+		if errors.Is(err, interfaces.ErrAlreadyExists) {
+			return c.String(http.StatusConflict, ShortURL)
+		} else {
+			return c.NoContent(http.StatusBadRequest)
+		}
 	}
+
 	return c.String(http.StatusCreated, ShortURL)
 }
 
@@ -82,11 +86,14 @@ func (s Server) PostJSON(c echo.Context) error {
 		return c.NoContent(http.StatusBadRequest)
 	}
 	response.ShortURL, err = s.shortenURL(userID, request.URL)
-	if errors.Is(err, interfaces.ErrAlreadyExists) {
-		return c.JSON(http.StatusConflict, response.ShortURL)
-	} else {
-		return c.NoContent(http.StatusBadRequest)
+	if err != nil {
+		if errors.Is(err, interfaces.ErrAlreadyExists) {
+			return c.JSON(http.StatusConflict, response.ShortURL)
+		} else {
+			return c.NoContent(http.StatusBadRequest)
+		}
 	}
+
 	return c.JSON(http.StatusCreated, response)
 }
 
