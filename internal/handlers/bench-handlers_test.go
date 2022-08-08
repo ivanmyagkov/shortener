0bb8c2db957776495f52a914b28c67065befa1c8
ivan_miagkov_1 <myagkovivan1@gmail.com>
2022-08-08 20:38:59 +0300

    increment 15

---
+++ internal/handlers/bench-handlers_test.go
@@ -0,0 +1,105 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/ivanmyagkov/shortener.git/internal/interfaces"
+	"github.com/ivanmyagkov/shortener.git/internal/utils"
+)
+
+type requestStruct struct {
+	URL string `json:"url"`
+}
+
+func BenchmarkServer_PostURL(b *testing.B) {
+	b.Run("PostURL", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			b.StopTimer()
+			id := utils.CreateID(16)
+			req, _ := http.NewRequest("POST", "http://localhost:9090/", strings.NewReader("http://www."+id+".com"))
+			client := &http.Client{}
+			b.StartTimer()
+			_, _ = client.Do(req)
+		}
+	})
+}
+
+func BenchmarkServer_PostJSON(b *testing.B) {
+	b.Run("PostJSON", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			b.StopTimer()
+			id := utils.CreateID(16)
+			url := requestStruct{URL: "http://www." + id + ".com"}
+			reqBody, _ := json.Marshal(url)
+			payload := strings.NewReader(string(reqBody))
+			req, _ := http.NewRequest("POST", "http://localhost:9090/api/shorten", payload)
+			client := &http.Client{}
+			b.StartTimer()
+			_, _ = client.Do(req)
+		}
+	})
+}
+
+func BenchmarkServer_PostBatch(b *testing.B) {
+	URLs := make([]interfaces.BatchRequest, 0, 100000)
+
+	for i := 0; i < cap(URLs); i++ {
+		id := utils.CreateID(16)
+		URLs = append(URLs, interfaces.BatchRequest{CorrelationID: "user", OriginalURL: "http://www." + id + ".com"})
+	}
+	b.ResetTimer()
+	b.Run("PostBatch", func(b *testing.B) {
+		b.StopTimer()
+		reqBody, _ := json.Marshal(URLs)
+		payload := strings.NewReader(string(reqBody))
+		req, _ := http.NewRequest("POST", "http://localhost:9090/api/shorten/batch", payload)
+		client := &http.Client{}
+		b.StartTimer()
+		_, _ = client.Do(req)
+	})
+}
+
+func BenchmarkServer_GetURLsByUserID(b *testing.B) {
+	b.Run("GetURLsByUserID", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			req, _ := http.NewRequest("GET", "http://localhost:9090/api/user/urls", nil)
+			client := &http.Client{}
+			b.StartTimer()
+			_, _ = client.Do(req)
+		}
+	})
+}
+
+func BenchmarkServer_GetURL(b *testing.B) {
+	b.Run("GetURL", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			req, _ := http.NewRequest("GET", "http://localhost:9090/c25d48c8b3a03c22", nil)
+			client := &http.Client{}
+			b.StartTimer()
+			_, _ = client.Do(req)
+		}
+	})
+}
+
+func BenchmarkServer_DelURLsBATCH(b *testing.B) {
+
+	URLs := make([]string, 0, 100000)
+
+	for i := 0; i < cap(URLs); i++ {
+		//id:= utils.CreateID(16)
+		URLs = append(URLs, "07175c915e801d21")
+	}
+	b.ResetTimer()
+	b.Run("DelURLsBATCH", func(b *testing.B) {
+		b.StopTimer()
+		reqBody, _ := json.Marshal(URLs)
+		payload := strings.NewReader(string(reqBody))
+		req, _ := http.NewRequest("DELETE", "http://localhost:9090/api/user/urls", payload)
+		client := &http.Client{}
+		b.StartTimer()
+		_, _ = client.Do(req)
+	})
+}
